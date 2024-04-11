de80df1339786096417f130d4eac6770e58f5d07
lindsey-trussell <97698864+lindsey-trussell@users.noreply.github.com>
2024-04-10 20:40:00 -0600

    updated wa12

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,39 +1,43 @@
-const quoteButton = document.querySelector('#js-new-quote');
-const answerButton = document.querySelector('#js-tweet');
+const bookButton = document.querySelector('#js-new-book');
+const linkButton = document.querySelector('#js-link-button');
 
-quoteButton.addEventListener('click', newQuote);
-answerButton.addEventListener('click', getAnswer);
+bookButton.addEventListener('click', newBook);
 
-var answerText = document.querySelector('#js-answer-text');
-var quoteText = document.querySelector('#js-quote-text');
+var descriptionText = document.querySelector('#js-book-description');
+var bookCover = document.querySelector('#js-book-cover');
 
-let answer = '';
+let bookLink = '';
 
-async function newQuote(){
-    console.log("quote works");
+async function newBook(){
+    console.log("hey look it's a function");
     try{
         const response = await fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=TYF4V2I58xEFOuhDjf486l7OHsO6ojew');
-        const quote = await response.json();
+        const object = await response.json();
+        const bookList = object.results['books'];
 
         if(!response.ok){
             throw Error(resonse.statusText);
         }
-        console.log(quote);
-        displayQuote(quote['question']);
-        answer = quote['answer'];
+        console.log(bookList);
+
+        const book = bookList[Math.floor(Math.random()*bookList.length)];
+
+        console.log(book);
+        displayBook(book);
+
+        console.log(book.buy_links[2].url);
+        bookLink = book.buy_links[2].url;
+        linkButton.setAttribute('href', bookLink);
 
     } catch (err){
         console.log(err);
         alert('nevermind, get fucked');
     }
-
-    answerText.textContent = '';
 } 
 
-function displayQuote(quote){
-    quoteText.textContent = quote;
+function displayBook(book){
+    bookCover.setAttribute('src', book['book_image']);
+    descriptionText.textContent = book['description'].toLowerCase();
 }
 
-function getAnswer(){
-    answerText.textContent = answer;
-}
+newBook();

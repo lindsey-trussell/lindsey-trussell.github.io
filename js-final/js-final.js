07b4008da1d512f1aa14c8a2e7924339547e022a
lindsey-trussell <97698864+lindsey-trussell@users.noreply.github.com>
2024-04-20 16:29:12 -0600

    added wa13

--- js-final/js-final.js
+++ js-final/js-final.js
@@ -1,39 +1,100 @@
-const btn = document.querySelector("button");
+// setup canvas
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-document.addEventListener("DOMContentLoaded", () => {
-  canvas.width = document.documentElement.clientWidth;
-  canvas.height = document.documentElement.clientHeight;
-});
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
 
-function random(number) {
-  return Math.floor(Math.random() * number);
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Ball {
+    constructor(x, y, velX, velY, color, size) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
     }
-    return color;
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+
+    update() {
+      if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+      }
+    
+      if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+      }
+    
+      if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+      }
+    
+      this.x += this.velX;
+      this.y += this.velY;
+    }
   }
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < 100; i++) {
-    ctx.beginPath();
-    ctx.fillStyle = randomColor();
-    ctx.arc(
-      random(canvas.width),
-      random(canvas.height),
-      i,
-      0,
-      2 * Math.PI,
-    );
-    ctx.fill();
-  }
+const balls = [];
+
+for(let i = 0; i <100; i++){
+  const size = i;
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-10, 10),
+    random(-10, 10),
+    randomRGB(),
+    size,
+  );
+  balls.push(ball);
+
+}
+while (balls.length < 15) {
+  const size = random(15, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-10, 10),
+    random(-10, 10),
+    randomRGB(),
+    size,
+  );
+  balls.push(ball);
 }
 
-btn.addEventListener("click", draw);
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 75%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();

9f83000cec49cea1e4589f59d4ad1e183571f206
lindsey-trussell <97698864+lindsey-trussell@users.noreply.github.com>
2024-05-04 17:02:54 -0600

    updated java final

--- js-final/js-final1.js
+++ js-final/js-final1.js
@@ -1,35 +1,302 @@
-var suits = ['cups','pentacles','swords','wands','trumpets'];
-var minor_names = ['one','two','three','four','five','six','seven','eitght','nine','ten','princess','queen','knight'];
-var major_names = ['The Fool', 'The Magician','The Priestess','The Empress','The Emperor','The Hierophant','The Lovers','Adjustment','The Hermit','Fortune','Lust','The Hanged Man','Death','Art','The Tower','The Star','The Moon','The Sun','The Aeon','The Universe'];
-var picked = [];
-var img_src;
+const tarot = [
+                    {
+                        "name": "The-Fool",
+                        "description": "The Fool is young and vulnerable. He has not yet experienced the ups and downs of life, leaving him unaware of the magnitude of life's challenges, as well as the potential he holds. Take on his open, willing energy and embrace all that lies ahead of you without worry.",
+                        "value": "100",
+                        "img": "img/fool.jpg"
+                    },
+                    {
+                        "name": "The-Magician",
+                        "description": "The Magician is a reminder that you are unique and have many gifts. These skills set you apart from the crowd and can help you begin new projects or overcome adversity. You already hold everything you need to move forward and accomplish what you've set out to do.",
+                        "value": "0",
+                        "img": "img/magician.jpg"
+                    },
+                    {
+                        "name": "The-High-Priestess",
+                        "description": "The most intuitive, connected card in the whole Tarot deck, The High Priestess is a card of awareness. This card urges you to listen to your inner voice and follow your instincts. Your mind knows far more than you think. Stop looking for answers in the outside world. Turn within for the guidance you seek.",
+                        "value": "-25",
+                        "img": "img/highPriestess.jpg"
+                    },
+                    {
+                        "name": "The-Empress",
+                        "description": "The Empress is the most feminine card in the Tarot, and greatly encourages compassion, beauty, and love. She is deeply connected to Mother Nature, and her influence is powerful when you absorb the energy of the natural world around you.",
+                        "value": "5",
+                        "img": "img/empress.jpg"
+                    },
+                    {
+                        "name": "The-Emporer",
+                        "description": "The Emperor is a card of leadership and power. He is an authoritative force who has been through many experiences to achieve this status. He represents structure and solidity, and reminds you that you, too, hold immense amounts of power over your own life, and what happens to it.",
+                        "value": "50",
+                        "img": "img/emporer.jpg"
+                    },
+                    {
+                        "name": "The-Hierophant",
+                        "description": "The Hierophant is like a messenger from the heavens. He is experienced in spirituality and guidance, and his job is to bring these lessons down to us here in the real world. Follow the rules, and find a spiritual perspective on your current situation.",
+                        "value": "25",
+                        "img": "img/hierophant.jpg"
+                    },
+                    {
+                        "name": "The-Lovers",
+                        "description": "The Lovers card represents the close relationships in your life. Your love life is in need of extra attention. However, this is just as much a card about your values and decisions. You may find The Lovers come up when you are at a crossroads. Consider all the possible consequences of your choices.",
+                        "value": "10",
+                        "img": "img/lovers.jpg"
+                    },
+                    {
+                        "name": "The-Chariot",
+                        "description": "The Chariot card is connected to your natural drive and determination, and can indicate an upcoming victory. This card reminds you that your greatest successes won't come through limited thinking -- when you combine the knowledge of your mind with that of your heart and spirit, you are an unstoppable force.",
+                        "value": "15",
+                        "img": "img/chariot.jpg"
+                    },
+                    {
+                        "name": "Strength",
+                        "description": "The Strength card represents courage, the fortitude of your heart, and your ability to withstand anything life hands you. Be reminded that you're strong enough to handle whatever you are facing and will come out of it with even more power than you had before. +100 to volume",
+                        "value": "100",
+                        "img": "img/strength.jpg"
+                    },
+                    {
+                        "name": "The-Hermit",
+                        "description": "The Hermit yearns to be alone. He knows that the only way to process what is happening in life is to withdraw from the noise of the world and create a quiet space of solitude. The answers you need will come from within. Be very still, and listen...",
+                        "value": "-100",
+                        "img": "img/hermit.jpg"
+                    },
+                    {
+                        "name": "Wheel-of-Fortune",
+                        "description": "The Wheel of Fortune is constantly revolving -- sometimes you will be at the top, and sometimes you will be at the bottom. Nothing is permanent, and, good or bad, you must cherish the lessons that this moment is bringing you.",
+                        "value": "-1",
+                        "img": "img/wheelOfFortune.jpg"
+                    },
+                    {
+                        "name": "Justice",
+                        "description": "Justice is your firm-but-fair reminder that there is a consequence for every action. Whatever life is handing you at this moment comes from decisions you and others have made in the past, reflecting natural cause-and-effect. Make sure you are acting fairly in all your interactions with others.",
+                        "value": "-25",
+                        "img": "img/justice.jpg"
+                    },
+                    {
+                        "name": "The-Hanged-Man",
+                        "description": "The Hanged Man tells you that sometimes small sacrifices are required to benefit the bigger picture. You likely want to make a move but don't know where to begin. Lightening your grip on what's no longer working for you, or detaching from the outcome of your situation, can help you release yourself.",
+                        "value": "-50",
+                        "img": "img/hangedMan.jpg"
+                    },
+                    {
+                        "name": "Death",
+                        "description": "One of the most misunderstood Tarot cards, Death is not a card about physical death. The Death card speaks of cycles and is a reminder that all things must pass. Hanging on to situations from the past will hinder you from allowing new, better things to enter your life. In every ending lies a chance for a new beginning.",
+                        "value": "-2",
+                        "img": "img/death.jpg"
+                    },
+                    {
+                        "name": "Temperance",
+                        "description": "The Temperance card is a master of moderation. She encourages peace and patience, and reminds you to go with the flow of your life instead of trying to force its pace or direction. Take things as they come, and remain flexible enough to change with the changes.",
+                        "value": "-10",
+                        "img": "img/temperance.jpg"
+                    },
+                    {
+                        "name": "The-Devil",
+                        "description": "The Devil card carries themes of restraint and powerlessness. You are likely feeling stuck. The Devil has convinced you that you have no options, but this couldn't be further from the truth. You hold the keys to your own freedom, but it's up to you to open the lock.",
+                        "value": "75",
+                        "img": "img/devil.jpg"
+                    },
+                    {
+                        "name": "The-Tower",
+                        "description": "The Tower represents destruction. It often comes up when everything in your life feels like its crumbling, and you have no way of stopping it. Just let it fall. The weakest parts of your life must be torn down in order to build something strong and sturdy in their place.",
+                        "value": "-75",
+                        "img": "img/tower.jpg"
+                    },
+                    {
+                        "name": "The-Star",
+                        "description": "The Star is the embodiment of hope and healing. She is a calming influence that brings messages of renewal, optimism, and inspiration. She reminds you that the universe is working in your favor, and encourages you to have faith in where you are being taken.",
+                        "value": "-25",
+                        "img": "img/star.jpg"
+                    },
+                    {
+                        "name": "The-Moon",
+                        "description": "The Moon card represents hidden thoughts, feelings, doubts and fears. You may be allowing your fears to override your faith in the future. Don't be deceived -- you can't believe everything that you see, hear, or think. If you can draw your feelings to the surface and address them, you can rid yourself of worry.",
+                        "value": "-50",
+                        "img": "img/moon.jpg"
+                    },
+                    {
+                        "name": "The-Sun",
+                        "description": "The Sun is a powerfully uplifting card, representing happiness, joy, vitality, and optimism. It is an positive sign that things are working well for you and that you're moving in the right direction. Lift your head and realize all the good situations and people that are surrounding you now and always.",
+                        "value": "50",
+                        "img": "img/sun.jpg"
+                    },
+                    {
+                        "name": "Judgement",
+                        "description": "Judgement is where your past and your future come together. You are being called on to review your decisions and your actions until now, to ensure they are in-line with where you ultimately want to go. Your future is not set in stone, and it's never too late to make a change for the better",
+                        "value": "20",
+                        "img": "img/judgement.jpg"
+                    },
+                    {
+                        "name": "The-World",
+                        "description": "As the last card of the Major Arcana, The World represents completion and fulfillment. You are exactly where you're meant to be. You have a greater understanding of who you are after all you've been through, and you're ready for the next phase of your journey.",
+                        "value": "0",
+                        "img": "img/world.jpg"
+                    }
+    ];
 
-console.log(suits);
-console.log(minor_names);
-console.log(major_names);
-
-function getRadndomInt(min, max){
-    return Math.floor(Math.random()*(max-min+1))+min;
-};
-
-function getCardName(suit, card_no){
-    var name = minor_names[card_no-1];
-    if(suit === 'trumps'){
-        name = major_names[card_no];
+function shuffleCards(shuffled, tarot){
+    console.log('shuffling');
+    if(shuffled.length <1){
+        for(let card of tarot){
+            shuffled.push(card);
+        }
     }
-};
-
-function shuffleCards(){
-    
-
+    for(let i = tarot.length - 1; i > 0; i--){
+        let j = Math.floor(Math.random() * (i+1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
 }
 
-function flipCard(){
+function loadCards(shuffled, cardContainer){
+    var pickedCards = document.querySelector('.picked-cards');
+    if(pickedCards.childElementCount > 0){
+        return;
+    }
+
+    while(cardContainer.firstChild){
+        for(let child of cardContainer.children){
+                child.remove();
+            }
+    }
+
+    for(let card of shuffled){
+        var cardDiv = '<div class="card-front"><img src="img/cardBack.jpg" alt="card back"></div><div class="card-back"><img ' + 'src = ' + card['img'] + ' alt = ' + card['name'] +'></div>';
+        var div = document.createElement("div");
+        div.setAttribute('class', 'card');
+        div.setAttribute('id', card['name']);
+        div.setAttribute('desc', card['description']);
+        div.setAttribute('value', card['value']);
+
+        div.innerHTML = cardDiv;
+        cardContainer.appendChild(div);
+    }
+    pickCard();
+}
+
+var volume = 50;
+var volDisplay = document.querySelector('h2');
+function setVolume(card){
+    volume += parseInt(card.getAttribute('value'));
+
+    if(volume > 100){
+        volume = 100;
+    }
+    if(volume < 0){
+        volume = 0;
+    }
+
+    volDisplay.textContent = 'volume: ' + volume;
+}
+
+function flipCard(pickedCards){
+
+    [...pickedCards].forEach((card)=>{
+    card.addEventListener( 'click', function() {
+        card.classList.add('is-flipped');
+
+        var desc = document.querySelector('#' + card.getAttribute('id') + '-desc');
+        desc.classList.add('is-visible');
+
+        console.log('oh shit here we are');
+        setVolume(card);
+        });
+        reset(pickedCards);
+    });
+}
+
+function reset(pickedCardsArr){
+    var shuffledCards = document.querySelector('.shuffled-cards');
+    var reset = document.querySelector('.reset');
+        reset.classList.add('is-visible');
+
+    reset.addEventListener('click', function(){
+        for(let card of pickedCardsArr){
+            card.remove();
+        }
+
+        var descContainer = document.querySelector('.desc');
+        while(descContainer.firstChild){
+            for(let child of descContainer.children){
+                child.remove();
+            }
+        }
+
+        var shuffled = [];
+        shuffleCards(shuffled, tarot);
+        loadCards(shuffled, shuffledCards);
+
+        reset.classList.remove('is-visible');
+    });
+}
+
+function pickCard(){
     var cards = document.querySelectorAll('.card');
+    var pickedCards = document.querySelector('.picked-cards');
+    var descContainer = document.querySelector('.desc');
+    var pickedCardsArr = [];
 
     [...cards].forEach((card)=>{
-        card.addEventListener( 'click', function() {
-        card.classList.toggle('is-flipped');
-    });
+            card.addEventListener( 'click', function() {
+                pickedCardsArr.push(card);
+                if(pickedCardsArr.length < 4){
+                    console.log('we got one');
+                    pickedCards.append(card);
+                    card.classList.add('is-picked');
+
+                    var paragraph = document.createElement("p");
+                    paragraph.setAttribute('id', card.getAttribute('id') + '-desc');
+                    paragraph.textContent = card.getAttribute('desc');
+
+                    descContainer.appendChild(paragraph);
+
+                    flipCard(pickedCardsArr);
+                }
+                else{
+                    return;
+                }
+        });
     });
 }
+
+function moveCard(pickedCard, newPos){
+    const pickedCardX = pickedCard.x;
+    const pickedCardY = pickedCard.y;
+    let newPosX = newPos.x;
+    let newPosY = newPos.y;
+ 
+    let moveToXPos = (pickedCardX - newPosX) * -1;
+    let moveToYPos = (pickedCardY - newPosY) * -1;
+
+    let translateX = 'translateX' + '(' + moveToXPos + 'px' + ')';
+    let translateY = 'translateY' + '(' + moveToYPos + 'px' + ')';
+    console.log(translateX);
+    console.log(translateY);
+
+    pickedCard.animate(
+        [
+            {transform: translateX},
+            {transform: translateY}
+        ],{
+            duration: 800,
+            easing: "ease-in-out"
+        });
+} 
+
+var cardContainer = document.querySelector('.shuffled-cards');
+shuffleButton = document.querySelector('.shuffle');
+
+window.onload = (event => {
+    console.log("page has loaded");
+
+    var shuffled = [];
+    shuffleCards(shuffled, tarot);
+    console.log(shuffled);
+    
+    loadCards(shuffled, cardContainer);
+
+    shuffleButton.addEventListener('click', function() {
+        shuffleCards(shuffled, tarot);
+        console.log(shuffled);
+        
+        loadCards(shuffled, cardContainer);
+    });
+});
